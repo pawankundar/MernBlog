2eedab7c396554c47a0d9cf9adc5d7ac884d8f7c
pawankundar <pawankundar98@hotmail.com>
2021-07-17 13:03:35 +0530

    Auth route completed

--- backEnd/routes/Auth.js
+++ backEnd/routes/Auth.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const {signUp} = require('../controller/Auth')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const { signUp, signIn } = require("../controller/Auth");
 
 //SIGN UP
 
-router.post('/signup',signUp)
-
+router.post("/signup", signUp);
 
 //SIGN IN
 
+router.post("/signin", signIn);
 
-
-module.exports = router
+module.exports = router;
