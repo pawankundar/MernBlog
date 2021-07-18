aa56cfb9bd466712bd539d2ae43dea803e2b7753
pawankundar <pawankundar98@hotmail.com>
2021-07-18 21:07:05 +0530

    added image upload using multer

--- backEnd/app.js
+++ backEnd/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const App = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const authRoute = require("./routes/Auth");
 const userRoute = require("./routes/Users");
+const postRoute = require("./routes/Posts");
+const categoryRoute = require("./routes/Categories");
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -15,9 +18,30 @@ mongoose
   .then(console.log("db Connected"))
   .catch((err) => console.log(err));
 
+//uploading image
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name); //"file.jpeg for postMan testing"
+  },
+});
+
+const upload = multer({ storage: storage });
+
+App.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json({
+    message: "file has been uploaded",
+  });
+});
+//
+
 App.use(express.json());
 App.use("/api/auth", authRoute);
 App.use("/api/users", userRoute);
+App.use("/api/posts", postRoute);
+App.use("/api/categories", categoryRoute);
 
 App.listen(process.env.PORT_NO, () => {
   console.log("server running");
