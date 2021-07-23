5333a542ac3baa8465ce2c15d160f883ac13db10
pawankundar <pawankundar98@hotmail.com>
2021-07-23 22:19:30 +0530

    post component

---
+++ frontend/src/Components/post/post.js
@@ -0,0 +1,43 @@
+require("./post.css");
+const Post = () => {
+  return (
+    <div className="Posts">
+      <img
+        className="postImage"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt="postImage"
+      />
+      <div className="postInfo">
+        <div className="categories">
+          <span className="category">Tech</span>
+          <span className="category">Art</span>
+          <span className="category">Music</span>
+        </div>
+        <span className="title">Demo Post Title</span>
+        <hr />
+        <span className="postDate">1 Day ago</span>
+      </div>
+      <p className="description">
+        
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+  );
+};
+
+export default Post;
