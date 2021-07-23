ddff0ccfcecc23531dfc180ac42dc6e86a3cafef
pawankundar <pawankundar98@hotmail.com>
2021-07-23 22:18:24 +0530

    header component completed

--- frontend/src/Components/header/Header.js
+++ frontend/src/Components/header/Header.js
@@ -1,5 +1,18 @@
+require('./Header.css')
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerTitle">
+        <span className="headerTitleSmall">React and Node</span>
+        <span className="headerTitleLarge">Blog</span>
+      </div>
+      <img
+        className="headerImage"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt="headerImage"
+      />
+    </div>
+  );
 };
 
 export default Header;
