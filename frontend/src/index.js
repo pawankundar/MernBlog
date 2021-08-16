0f35a68fc0490e290249aa750f521a8101bda59f
pawankundar <pawankundar98@hotmail.com>
2021-08-16 20:53:30 +0530

    added context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
-import reactDom from 'react-dom'
-import App from './App'
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App";
+import { ContextProvider } from "./context/Context";
 
-reactDom.render(<App/>,document.getElementById('root'))
+reactDom.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
