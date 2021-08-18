86c129e7ed52aae4e74c8183dcb6f7f452275835
pawankundar <pawankundar98@hotmail.com>
2021-08-18 22:29:12 +0530

    saving user to localStorage

--- frontend/src/context/Context.js
+++ frontend/src/context/Context.js
@@ -1,26 +1,30 @@
-import { createContext, useReducer } from "react"
-import Reducer from "./Reducer"
+import { createContext, useEffect, useReducer } from "react";
+import Reducer from "./Reducer";
 
 const INITIAL_STATE = {
-    user : null,
-    isFetching : false,
-    error : false
-}
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
 
-export const Context = createContext(INITIAL_STATE)
+export const Context = createContext(INITIAL_STATE);
 
-export const ContextProvider = ({children}) =>{
-    const [state,dispatch] = useReducer(Reducer,INITIAL_STATE)
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+  useEffect(()=>{
+      localStorage.setItem("user",JSON.stringify(state.user))
+  },[state.user])
 
-    return (
-        <Context.Provider value={{
-            user:state.user,
-            isFetching:state.isFetching,
-            error:state.error,
-            dispatch
-        }}>
-            {children}
-
-        </Context.Provider>
-    )
-}
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
