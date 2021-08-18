bbc3f635fd9ba25bb2264cb7e66d45889b3aa88c
pawankundar <pawankundar98@hotmail.com>
2021-08-18 22:34:18 +0530

    retrieving posts from db

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,16 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../../Components/header/Header";
 import React from "react";
 import SideBar from "../../Components/sideBar/sideBar";
 import Posts from "../../Components/posts/posts";
+import { useLocation } from "react-router-dom";
 
 require("./Home.css");
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts" + search);
+      setData(res.data);
+    };
+    getPost();
+  }, [search]);
   return (
     <>
       <Header />
       <div className="home">
-       <Posts/>
+        <Posts data={data} />
         <SideBar />
       </div>
     </>
