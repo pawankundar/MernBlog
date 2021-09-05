ea7005853f32ebf494906a5c27c2fc3135ad7525
pawankundar <pawankundar98@hotmail.com>
2021-09-05 15:38:02 +0530

    Added edit post functionality

--- frontend/src/Components/singlePost/singlePost.js
+++ frontend/src/Components/singlePost/singlePost.js
@@ -10,39 +10,77 @@ const SinglePost = () => {
   const [post, setPost] = useState({});
   const { user } = useContext(Context);
 
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [updateMode, setupdateMode] = useState(false);
+
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get("/posts/" + path);
       setPost(res.data);
+      setTitle(res.data.title);
+      setDescription(res.data.description);
     };
 
     getPost();
   }, [path]);
 
-  const deletePost = () => {
-    axios
+  const deletePost = async () => {
+    await axios
       .delete(`/posts/${path}`, {
         data: { username: user.username },
       })
-      .then((resp) => window.location.replace("/"))
-      .catch((err) => console.log("error in deletePost"));
+      .then(() => window.location.replace("/"))
+      .catch(() => console.log("error in deletePost"));
   };
-  const ImageLink = "http://localhost:8000/images/"
-  const updatePost = () => {};
 
+  const updatePost = async () => {
+    await axios
+      .put(`/posts/${path}`, {
+        username: user.username,
+        title,
+        description,
+      })
+      .then(() => setupdateMode(false))
+      .catch(() => console.log("error in updatePost"));
+  };
+  
+  const ImageLink = "http://localhost:8000/images/";
   return (
     <div className="singlePost">
       <div className="wrapper">
-        {post.photo && <img className="postImage" src={ ImageLink +post.photo +".jpg"} alt="" />}
-        <h1 className="postTitle">
-          {post.title}
-          {user.username === post.username && (
-            <div className="EditButtons">
-              <i className="editIcon far fa-edit" />
-              <i className="editIcon far fa-trash-alt" onClick={deletePost} />
-            </div>
-          )}
-        </h1>
+        {post.photo && (
+          <img
+            className="postImage"
+            src={ImageLink + post.photo + ".jpg"}
+            alt=""
+          />
+        )}
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="postTitleEdit"
+            autoFocus
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        ) : (
+          <h1 className="postTitle">
+            {title}
+            {user?.username === post.username && (
+              <div className="EditButtons">
+                <i
+                  className="editIcon far fa-edit"
+                  onClick={() => setupdateMode(true)}
+                />
+                <i className="editIcon far fa-trash-alt" onClick={deletePost} />
+              </div>
+            )}
+          </h1>
+        )}
+
         <div className="PostInfo">
           <span>
             Author :
@@ -54,8 +92,30 @@ const SinglePost = () => {
             {new Date(post.createdAt).toDateString()}
           </span>
         </div>
-
-        <p className="postDescription">{post.description}</p>
+        {updateMode ? (
+          <input
+            value={description}
+            className="postDescriptionEdit"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        ) : (
+          <p className="postDescription">{description}</p>
+        )}
+        {updateMode && (
+          <div className="updateButtonDiv">
+            <button
+              className="discardPostButton"
+              onClick={() => {
+                setupdateMode(false);
+              }}
+            >
+              Discard
+            </button>
+            <button className="updatePostButton" onClick={updatePost}>
+              Update
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
